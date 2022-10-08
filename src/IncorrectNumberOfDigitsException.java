33189e56a4d1364639aeb3474bf104a8479ff9f3
SimiJoseph <87762212+simijoseph7@users.noreply.github.com>
2022-10-08 23:46:07 +1300

    Delete IncorrectNumberOfDigitsException.java

--- src/IncorrectNumberOfDigitsException.java
+++
@@ -1,7 +0,0 @@
-package bullsandcows;
-
-public class IncorrectNumberOfDigitsException extends Exception{
-    public IncorrectNumberOfDigitsException(String message) {
-        super(message);
-    }
-}
